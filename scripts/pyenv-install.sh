4857913286a88a18f33405b7c997805bef98caa2
RenoirTan <renoirtan2005@gmail.com>
2021-08-16 21:53:58 +0800

    main: add pyenv installer

---
+++ scripts/pyenv-install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv &&
+cd ~/.pyenv && src/configure && make -C src &&
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile &&
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile &&
+echo 'eval "$(pyenv init --path)"' >> ~/.profile &&
