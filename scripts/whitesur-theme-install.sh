4b52ec736ba56908693f9f77b7d7ef7bcfc80223
RenoirTan <renoirtan2005@gmail.com>
2021-08-16 22:50:53 +0800

    main: install whitesur to /usr/share/themes

--- scripts/whitesur-theme-install.sh
+++ scripts/whitesur-theme-install.sh
@@ -3,4 +3,4 @@
 
 git clone git@github.com:vinceliuice/WhiteSur-gtk-theme.git ~/Code/github/vinceliuice/WhiteSur-gtk-theme &&
 cd ~/Code/github/vinceliuice/WhiteSur-gtk-theme &&
-./install.sh --theme all -N glassy
+./install.sh --theme all --nautilus-style glassy --dest /usr/share/themes
