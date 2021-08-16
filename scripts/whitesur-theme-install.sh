7cf370b81cc2c15d7b04697e5a47e951566d2ed0
RenoirTan <renoirtan2005@gmail.com>
2021-08-16 22:47:17 +0800

    main: add whitesur theme installer

---
+++ scripts/whitesur-theme-install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+
+git clone git@github.com:vinceliuice/WhiteSur-gtk-theme.git ~/Code/github/vinceliuice/WhiteSur-gtk-theme &&
+cd ~/Code/github/vinceliuice/WhiteSur-gtk-theme &&
+./install.sh --theme all -N glassy
