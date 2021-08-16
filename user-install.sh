f21f417fe316853fafce68221870312beeb1bcd2
RenoirTan <renoirtan2005@gmail.com>
2021-08-16 21:56:29 +0800

    main: add user-install.sh

---
+++ user-install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+
+CHECKLIST_FOLDER="$(dirname $0)"
+SCRIPTS_FOLDER="$CHECKLIST_FOLDER/scripts"
+
+
+source "$SCRIPTS_FOLDER/utils.sh"
+
+
+vinstall_package -c "pyenv" -a "pyenv" -f "$SCRIPTS_FOLDER/pyenv-install.sh"
