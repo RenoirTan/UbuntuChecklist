0135bd272a3be28a5e64b43bea0d1f23f01334ba
RenoirTan <renoirtan2005@gmail.com>
2021-08-17 12:55:57 +0800

    main: remove erroneous '&&' at the end

--- scripts/pyenv-install.sh
+++ scripts/pyenv-install.sh
@@ -5,4 +5,4 @@ git clone https://github.com/pyenv/pyenv.git ~/.pyenv &&
 cd ~/.pyenv && src/configure && make -C src &&
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile &&
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile &&
-echo 'eval "$(pyenv init --path)"' >> ~/.profile &&
+echo 'eval "$(pyenv init --path)"' >> ~/.profile
