89e2c9a42523fc214660aeffe1cbdfa0ca830b59
RenoirTan <renoirtan2005@gmail.com>
2021-08-22 10:48:11 +0800

    main: add extensions installer

---
+++ scripts/vscode-extensions.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/bash
+
+
+SCRIPTS_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+source "$SCRIPTS_FOLDER/utils.sh"
+
+
+if [ $(command_exists "code"; echo "$?") -eq 0 ]; then
+    echo "vscode not installed"
+    exit 1
+fi
+
+
+function vinstall_extension {
+    while [ "$#" -gt 0 ]; do
+        echo "Installing $1"
+        code --install-extension "$1" --force
+        shift
+    done
+}
+
+vinstall_extension \
+    "donjayamanne.python-extension-pack" \
+    "ms-vscode.cpptools" \
+    "twxs.cmake" \
+    "renoirtan.liver" \
+    "mhutchie.git-graph" \
+    "eamodio.gitlens" \
+    "rust-lang.rust" \
+    "matklad.rust-analyzer" \
+    "golang.go" \
+    "adpyke.codesnap" \
+    "helgardrichard.helium-icon-theme" \
+    "miguelsolorio.fluent-icons"
